013e3d11a549c0db4cec401ff4b38f6d900a6331
Jackson Kontny <jackson.kontny@gopuff.com>
2022-09-04 11:47:24 -0500

    black cleanup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 import time
 
-from src.env import APPROVAL_USERS, REPO_OWNER, APPROVALS_REQUIRED, INTERVAL_IN_SECONDS, TITLE_REGEX, REPO
+from src.env import (
+    APPROVAL_USERS,
+    REPO_OWNER,
+    APPROVALS_REQUIRED,
+    INTERVAL_IN_SECONDS,
+    TITLE_REGEX,
+    REPO,
+)
 from src.gh.api import get_pulls, should_approve_pull, submit_pr_review
 
 
@@ -8,15 +15,16 @@ def run():
     while True:
         open_prs = get_pulls(REPO, owner=REPO_OWNER)
         prs_to_approve = [
-            pr for pr in open_prs
+            pr
+            for pr in open_prs
             if should_approve_pull(pr, APPROVAL_USERS, APPROVALS_REQUIRED)
         ]
         if len(prs_to_approve) == 0:
-            print('No PRs to approve...')
+            print("No PRs to approve...")
         for pr in prs_to_approve:
             print(f'APPROVING PR: "{pr.title}" for user: "{pr.username}"')
             submit_pr_review(pr, owner=REPO_OWNER)
-            print(f'APPROVED PR: {pr.title} for user: {pr.username}')
+            print(f"APPROVED PR: {pr.title} for user: {pr.username}")
         time.sleep(INTERVAL_IN_SECONDS)
 
 
